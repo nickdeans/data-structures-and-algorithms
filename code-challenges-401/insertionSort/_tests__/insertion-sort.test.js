8977fa5b47366438fd27a2198d1477abb5648130
nickdeans <nickabramowicz@gmail.com>
2021-03-23 15:28:08 -0700

    completed code challenge 26

---
+++ code-challenges-401/insertionSort/_tests__/insertion-sort.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const insertionSort = require('../insertion-sort.js');
+
+describe('Tests for Insertion Sort function', () => {
+    it('should sort the array values from least to greatest', () => {
+        let testArray = [8,4,23,42,16,15];
+        let test = insertionSort(testArray);
+
+        expect(test).toEqual([4,8,15,16,23,42])
+    })
+
+    it('should sort an array from greatest to lowest to lowest ot greatest', () => {
+        let testArray = [50,40,30,20,10];
+        let test = insertionSort(testArray);
+
+        expect(test).toEqual([10,20,30,40,50]);
+    })
+})
